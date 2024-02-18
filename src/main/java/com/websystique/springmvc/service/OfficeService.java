d4c43e33323b413fb17b5a82e3d611d0a310e10c
Elena <michailova.alena@gmail.com>
2024-02-18 20:33:23 +0300

    first commit

---
+++ src/main/java/com/websystique/springmvc/service/OfficeService.java
@@ -0,0 +1,24 @@
+package com.websystique.springmvc.service;
+
+
+import com.websystique.springmvc.model.Office;
+import com.websystique.springmvc.model.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+
+public interface OfficeService {
+    List<Office> findAllOffices();
+
+    Office findById(int id);
+
+
+    void saveOffice(Office office);
+
+    void updateOffice(Office office);
+
+    void deleteOfficeById(int id);
+
+}
